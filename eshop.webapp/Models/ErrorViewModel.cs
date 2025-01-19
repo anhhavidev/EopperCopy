a525856bd01f652228a82c99e5920de3c8d46bb6
Admin <Admin@DESKTOP-MEKGRP5>
2025-01-19 08:32:01 -0800

    thêm get setter

--- eshop.webapp/Models/ErrorViewModel.cs
+++ eshop.webapp/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace eshop.webapp.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-        public int id;
+        public int id { get; set; }
         // lấy từ git về trường name nhé
         public string name {get ;set;}
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
