114b2bf8e90e8e3202cf5bd9afdaab2ea50f35bf
Admin <Admin@DESKTOP-MEKGRP5>
2025-01-19 08:24:11 -0800

    thêm trường id

--- eshop.webapp/Models/ErrorViewModel.cs
+++ eshop.webapp/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace eshop.webapp.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        public int id;
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
